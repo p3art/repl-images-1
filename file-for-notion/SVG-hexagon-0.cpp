f2f708da5a58023e8216522b5c8613ff26754d67
幾何藝術與天文星圖 (Python 程式設計與運算思維) <37860460+p3art@users.noreply.github.com>
2026-02-26 14:04:30 +0800

    update SVG-hexagon-0.cpp

---
+++ file-for-notion/SVG-hexagon-0.cpp
@@ -0,0 +1,30 @@
+/*
+All/01-nix-cpp-SVG/旗山中山路95號-Hexagon/
+SVG-hexagon-0
+//
+2024-08-17
+2025-09-13
+//
+run on replit.com web platform
+1MiB = 1.048MB
+
+開天闢地，自創始之初，我們寫一個小小的程式，
+測試整個運行的環境是否正確無誤，如我們所預期的...
+*/
+//
+#include <iostream>
+using namespace std;
+//
+//remember that :: is the scope resolving operator 場域解析運算子
+//
+int main() {
+    cout << endl;
+    cout << "main() running...." << endl;
+    cout << "done" << endl;
+    cout << "exit main" << endl;
+    cout << "exit shell" << endl;
+    cout << endl;
+    //
+    return 0;
+}
+//
