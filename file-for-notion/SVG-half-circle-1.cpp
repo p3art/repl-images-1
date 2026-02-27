ee4fdde8eeeaca265ac900d6050162a8df3532cf
幾何藝術與天文星圖 (Python 程式設計與運算思維) <37860460+p3art@users.noreply.github.com>
2026-02-28 04:07:05 +0800

    update SVG-half-circle-1.cpp

---
+++ file-for-notion/SVG-half-circle-1.cpp
@@ -0,0 +1,26 @@
+/*
+All/01-nix-cpp-SVG/半圓對對碰/
+SVG-half-circle-1
+//
+2024-08-09
+2025-09-13
+//
+run on replit.com web platform
+1MiB = 1.048MB
+*/
+//
+#include <iostream>
+using namespace std;
+//
+//remember that :: is the scope resolving operator
+//
+int main() {
+    cout << endl;
+    cout << "main() running...." << endl;
+    cout << "done" << endl;
+    cout << "exit main()..." << endl;
+    cout << endl;
+    //
+    return 0;
+}
+//
